0f3a705c3718cc2d53e30f8e15c2b50a1f61695b
guozhaosengzs <guozhaosengzs@yahoo.com>
2020-10-16 04:05:34 -0400

    1

--- project.R
+++ project.R
@@ -1,18 +1,33 @@
 library(tidyverse)
 library(caTools)
-library(funModeling)
 
 data <- read_csv('https://raw.githubusercontent.com/guozhaosengzs/LoL_strategy/master/high_diamond_ranked_10min.csv')
 
+# Train : Test = 0.75 : 0.25
 set.seed(12345)
 data$spl <- sample.split(seq_len(nrow(data)), SplitRatio = 0.75)
 
 data.train <- subset(data, spl == TRUE)
 data.test <- subset(data, spl == FALSE)
 
-sum(data.train$blueWins) / nrow(data.train)
+# First blood took place every game beofre 10min 
+x <- data.train$blueFirstBlood - data.train$redFirstBlood
+unique(x)
 
 
+# Remake a less confusing data set
+colnames(data.train)
+data.train.lite <- data.train %>% 
+  select(blueWins, blueFirstBlood, blueKills, blueDeaths, blueDragons, blueGoldDiff, blueExperienceDiff)
+
+data.train.lite$killParticipationDiff <- data.train$blueAssists - data.train$redAssists
+data.train.lite$visionScore <- 
+  data.train$blueWardsPlaced + data.train$blueWardsDestroyed - data.train$redWardsPlaced - data.train$redWardsDestroyed
+
+# First try at a logistic regression model
+
+model1 <- glm(data = data.train.lite, blueWins ~., family=binomial())
+summary(model1)
 
 
-
+# Multinomial logistic regression?
